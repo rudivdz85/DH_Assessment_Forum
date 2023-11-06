595becf50e150f121fe8996630826b2d7f543b71
Rudi van der Zee <rudiv85@gmail.com>
2023-11-06 22:06:35 +0200

    Added Models

--- forum_app/models.py
+++ forum_app/models.py
@@ -1,3 +1,23 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('post', 'user')
+
+    def __str__(self):
+        return f"{self.user} likes {self.post}"
